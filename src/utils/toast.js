cb673f669e35372c6c5b43f4e399fc72863515ff
eatingtiger-bitew <eatingtiger@gmail.com>
2026-06-29 08:34:45 +0800

    Delete src directory

--- src/utils/toast.js
+++
@@ -1,9 +0,0 @@
-let _toastTimer;
-export function toast(msg) {
-  const el = document.getElementById('toast');
-  if (!el) return;
-  el.textContent = msg;
-  el.classList.add('show');
-  clearTimeout(_toastTimer);
-  _toastTimer = setTimeout(() => el.classList.remove('show'), 2400);
-}
